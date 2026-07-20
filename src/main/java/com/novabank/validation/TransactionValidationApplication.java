aa0f655e54c9800947068d7e825f46fc474e4698
hrithikraina <62991898+hrithikraina@users.noreply.github.com>
2026-07-20 17:03:19 +0530

    Update TransactionValidationApplication.java

--- src/main/java/com/novabank/validation/TransactionValidationApplication.java
+++ src/main/java/com/novabank/validation/TransactionValidationApplication.java
@@ -7,6 +7,7 @@ public class TransactionValidationApplication {
     public static void main(String[] args) throws Exception {
         ValidationLog log = new ValidationLog();
         TransactionValidationService service = new TransactionValidationService(new RiskRuleEngine(), log);
+        System.out.prinln("PAY-482912 is supposed to be filtered, validation");
         try { service.validate(new PaymentForValidation("PAY-90021", 1250.00)); }
         catch (IndexOutOfBoundsException error) { log.error("java.lang.IndexOutOfBoundsException: " + error.getMessage() + " while selecting sanctions rule for payment_id=PAY-90021"); }
         Files.createDirectories(Path.of("logs")); Files.writeString(Path.of("logs/transaction-validation-service.log"), log.entries()); System.out.print(log.entries());
